7ad1863b474a8d71bfa73b5c8dade8f924202e5a
Rich Hanna <codingninja84@gmail.com>
2017-06-07 08:06:14 -0700

    latest changes

--- public/src/app/main-content/main-content.component.ts
+++ public/src/app/main-content/main-content.component.ts
@@ -1,4 +1,4 @@
-import { Component,  OnInit, trigger, state, style, transition, animate, HostListener } from '@angular/core';
+import { Component,  OnInit, trigger, state, style, transition, animate, HostListener, keyframes } from '@angular/core';
 import { IntroService } from '../intro/intro.service'
 @Component({
   selector: 'app-main-content',
@@ -68,14 +68,81 @@ import { IntroService } from '../intro/intro.service'
    transition('inactive => active', animate('1000ms ease-in-out')),
    transition('active => inactive', animate('100ms ease-in-out')),
   ]),
-  ]
+  trigger('contactModal', [
+    state('inactive', style({
+      transform: "translateX(-100%)",
+      display: "none",
+    })),
+    state('active', style({
+      display : "flex",
+    })),
+    transition('inactive => active', animate('400ms ease-in-out')),
+    transition('active => inactive', animate('100ms ease-in-out')),
+   ]),
+   trigger('contact', [
+     state('inactive', style({
+       transform: "translateX(0)",
+     })),
+     state('active', style({
+      //  transform: "translateX(-500%)"
+      zIndex: "9500",
+      position: "fixed",
+      left: "10vw",
+      top: "5vh",
+      height: "10vw",
+      width: "10vw",
+      clipPath: "polygon(20% 0%, 0% 20%, 30% 50%, 0% 80%, 20% 100%, 50% 70%, 80% 100%, 100% 80%, 70% 50%, 100% 20%, 80% 0%, 50% 30%)"
+     })),
+     transition('inactive => active', [
+      animate(500, keyframes([
+        style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
+        style({opacity: 1, transform: 'translateX(-15px)', offset: 0.7}),
+        style({opacity: 0, transform: 'translateX(100%)',  offset: 1.0})
+      ])),
+     transition('active => inactive', animate('1000ms ease-in-out')),
+    ]),
+  ]),
+  trigger('playSubmit', [
+    state('void', style({
+      transform: "translatex(100%)",
+      opacity: "0"
+    })),
+    transition('active => void', animate('1000ms ease-in-out')),
+   ]),
+   trigger('thumbUp', [
+     state('inactive', style({
+       display: "none",
+       opacity: "0"
+     })),
+     state('active', style({
+       display: "flex"
+     })),
+     transition('inactive <=> active', [
+      animate(300, keyframes([
+        style({opacity: 1, transform: 'scale(.75)', offset: 0.7}),
+        style({opacity: 1, transform: 'scale(1.2)',  offset: 1.0})
+      ])),
+    ])
+  ])
+]
 })
 export class MainContentComponent implements OnInit {
   menuState = 'inactive';
   triState = 'inactive';
-  formState = "inactive";
-  stickyState = "inactive";
+  formState = 'inactive';
+  stickyState = 'inactive';
+  contactState = 'inactive';
+  thumbState = "inactive";
+  contact = 'Contact';
+  submit = 'active';
   menu: any;
+
+  //form inputs
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  message: string;
 
   constructor(private introService: IntroService) { }
 
@@ -98,6 +165,16 @@ export class MainContentComponent implements OnInit {
   }
 
 
+  clearForm(){
+    this.firstName = "";
+    this.lastName = "";
+    this.phone = "";
+    this.email = "";
+    this.message = "";
+  }
+
+  //menu & form toggles
+
   toggleMenu(){
     this.menuState = this.menuState == "inactive" ? "active" : "inactive";
   }
@@ -115,5 +192,16 @@ export class MainContentComponent implements OnInit {
     location.reload()
   }
 
+  toggleSidebar(){
+    this.contactState = this.contactState == "inactive" ? "active" : "inactive";
+    this.contact = this.contact == "Contact" ? "Close" : "Contact";
+  }
 
+  toggleSubmit(){
+    this.submit = "void";
+    setTimeout(()=>{
+      this.thumbState = "active";
+      setTimeout(()=>{this.toggleForm()},3000);
+  },1000)
+  }
 }
