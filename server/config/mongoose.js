715729f99a5d4bd6e71a5d4030ddb350bac83929
Rich Hanna <codingninja84@gmail.com>
2017-06-12 07:42:37 -0700

    updated server file

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var path = require('path');
 
 //Uncomment when ready to use DB
-// mongoose.connect('mongodb://localhost/form');
+mongoose.connect('mongodb://localhost/form');
 var models_path = path.join(__dirname, './models');
 
 fs.readdirSync(models_path).forEach(function(file) {
