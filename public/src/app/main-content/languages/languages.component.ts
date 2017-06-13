bb76cbfbf940caff7820600091bbc1f7e912b1ad
Rich Hanna <codingninja84@gmail.com>
2017-06-12 20:53:47 -0700

    almost done

--- public/src/app/main-content/languages/languages.component.ts
+++ public/src/app/main-content/languages/languages.component.ts
@@ -8,6 +8,7 @@ import { Component,  OnInit, trigger, state, style, transition, animate, HostLis
 })
 export class LanguagesComponent implements OnInit {
   lang: string = "";
+  aboutLang: string = "";
   description: string[] = [];
   counter: number = 0;
   active: any;
@@ -15,10 +16,10 @@ export class LanguagesComponent implements OnInit {
   slideTimeout: any;
   languageArray = ["JavaScript","TypeScript","Swift","Python"];
   language: any = {
-    "JavaScript":  ["ExpressJS", "NodeJS", "Sockets", "Bower", "NPM" ],
-    "TypeScript":  ["Angular2", "Backbone", "React"],
-    "Swift":  ["Xcode", "Foundation", "Core-Motion", "Core-Data", "Auto-Layout"],
-    "Python":  ["Django", "Flask", "Virtualenv", "Pip"]
+    "JavaScript":  { frameworks: ["ExpressJS", "NodeJS", "Jquery", "Sockets", "Bower", "NPM" ], about: "Whether using MongoDB for data storage, Node.js for the web backend, jQuery/Angular/React/Ember.js for the frontend, undeniably, Javascript is Ubiqutous. This means that mastery of JavaScript lets you start small and build fun things, but can take you all the way. No other programming language today can claim that."},
+    "TypeScript":  { frameworks: ["Angular2", "Backbone", "React"],  about: "This is JavaScript without a lot of the looseness of JS. It uses ES6 type syntax and super fun to write in! This portfolio is actually built using Angular2, and some of the best web apps I have built are created using TypeScript"},
+    "Swift":  { frameworks: ["Xcode", "Foundation", "Core-Motion", "Core-Data", "Auto-Layout"], about: "Objective-C, without the C. Apple's own words when they unveiled this Static Language. It's a blast to write in! X-code compiles as you type and the storyboard makes build Auto-Layouts a breeze! Mobile app development has never been so easy!"},
+    "Python":  { frameworks:  ["Django", "Flask", "Virtualenv", "Pip"],about: "Python is a general-purpose language, it can be used to build just about anything. I use Python because it is great for backend web development, data analysis, artificial intelligence, and scientific computing. There are plenty of resources to help learn how to do those as well."},
   }
   constructor() { }
 
@@ -33,20 +34,23 @@ export class LanguagesComponent implements OnInit {
 
   slider(){
     this.lang = this.languageArray[this.counter];
-    this.description = this.language[this.languageArray[this.counter]];
+    this.description = this.language[this.languageArray[this.counter]].frameworks;
+    this.aboutLang = this.language[this.languageArray[this.counter]].about
     this.setActive(null);
     if(this.counter < this.languageArray.length - 1) {
       this.removeActive();
       this.counter++;
       this.setActive(null);
       this.lang = this.languageArray[this.counter];
-      this.description = this.language[this.languageArray[this.counter]];
+      this.description = this.language[this.languageArray[this.counter]].frameworks;
+      this.aboutLang = this.language[this.languageArray[this.counter]].about
     } else {
       this.removeActive();
       this.counter = 0;
       this.setActive(null);
       this.lang = this.languageArray[this.counter];
-      this.description = this.language[this.languageArray[this.counter]];
+      this.description = this.language[this.languageArray[this.counter]].frameworks;
+      this.aboutLang = this.language[this.languageArray[this.counter]].about;
     }
     this.slideTimeout = setTimeout(()=>{this.slider()},4000);
     }
@@ -62,12 +66,14 @@ export class LanguagesComponent implements OnInit {
         if(idx == 0){
           this.counter = 0;
           this.lang = this.languageArray[idx];
-          this.description = this.language[this.languageArray[idx]];
+          this.description = this.language[this.languageArray[idx]].frameworks;
+          this.aboutLang = this.language[this.languageArray[idx]].about
           this.boxes[idx].classList.add('active');
         } else {
           this.counter = idx;
           this.lang = this.languageArray[idx];
-          this.description = this.language[this.languageArray[idx]];
+          this.description = this.language[this.languageArray[idx]].frameworks;
+          this.aboutLang = this.language[this.languageArray[idx]].about
           this.boxes[idx].classList.add('active')
         }
       }
