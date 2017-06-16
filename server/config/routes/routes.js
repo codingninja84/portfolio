753165c2b936665db5b3cfa20a1c267249fe33c1
Rich <rhanna1461@hotmail.com>
2017-06-16 04:19:21 +0000

    pulling changes

--- server/config/routes/routes.js
+++ server/config/routes/routes.js
@@ -2,13 +2,11 @@ var emailHandler = require('../controllers/email-handler.js')
 
 module.exports = function(app) {
 
-<<<<<<< HEAD
 module.exports = function(req, res) {
-	  
-=======
+
   app.post('/contactForm', function(req, res) {
     emailHandler.sendEmail(req.body);
     res.json("Email Has been sent via SMTP")
   })
->>>>>>> f6b91eea5aa78493b553fb5149598626032c1c83
+
 }
