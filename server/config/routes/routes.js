47f49d642fb10e4744f7bf9a3f5573ef5841ba8d
Rich Hanna <codingninja84@gmail.com>
2017-06-15 22:41:04 -0700

    Fixing mobile form overflow bug

--- server/config/routes/routes.js
+++ server/config/routes/routes.js
@@ -3,6 +3,7 @@ var emailHandler = require('../controllers/email-handler.js')
 module.exports = function(app) {
 
   app.post('/contactForm', function(req, res) {
+    console.log("in routes");
     emailHandler.sendEmail(req.body);
     res.json("Email Has been sent via SMTP")
   })
